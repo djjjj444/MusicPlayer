770826e8871053cc35486be4d5f7da834d243aa9
zhanglin-ui <1475762461@qq.com>
2019-12-10 23:24:41 +0800

    seekBar实时更新

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -5,23 +5,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.net.wifi.aware.DiscoverySession;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity{
+
     private MediaPlayer mediaPlayer=new MediaPlayer();
+    private SeekBar seekBar;
+    private Timer timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,10 +41,14 @@ public class MainActivity extends AppCompatActivity{
         ListAdepter listAdepter=new ListAdepter(this,list);
         ListView lv=(ListView)findViewById(R.id.lv);
         lv.setAdapter(listAdepter);
+        final Handler handler=new Handler();
         Button play=(Button)findViewById(R.id.play);
         Button pause=(Button)findViewById(R.id.pause);
         Button stop=(Button)findViewById(R.id.stop);
-        final SeekBar seekBar=(SeekBar)findViewById(R.id.seekbar);
+        final TextView tv1=(TextView)findViewById(R.id.tv1);
+        final TextView tv2=(TextView)findViewById(R.id.tv2);
+        seekBar=(SeekBar)findViewById(R.id.seekbar);
+
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -47,19 +62,22 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                mediaPlayer.seekTo(seekBar.getProgress());
             }
         });
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Song song = list.get(position);
+                seekBar.setMax(song.duration);
+                seekBar.setProgress(0);
+                tv1.setText(song.song);
+                tv2.setText(song.singer);
                 if (!mediaPlayer.isPlaying()) {
                     try {
                         mediaPlayer.setDataSource(song.path);
                         mediaPlayer.prepare();
                         mediaPlayer.start();
-                        seekBar.setMax(song.duration);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -73,7 +91,9 @@ public class MainActivity extends AppCompatActivity{
                         e.printStackTrace();
                     }
                     mediaPlayer.start();
+                    seekBar.setProgress(0);
                 }
+
             }
         });
         play.setOnClickListener(new View.OnClickListener(){
@@ -87,8 +107,18 @@ public class MainActivity extends AppCompatActivity{
                 }
             }
         });
+        seekProcess();
 
     }
 
-
+    public void seekProcess(){
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                int i=mediaPlayer.getCurrentPosition();
+                seekBar.setProgress(i);
+            }
+        },0,200);
+    }
 }
