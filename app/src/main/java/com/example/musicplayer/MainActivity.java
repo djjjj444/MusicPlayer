4b95d7db06cd683bfc44705c5a7fe96ec94d7a30
zhanglin-ui <1475762461@qq.com>
2019-12-11 13:08:44 +0800

    随机播放和顺序播放

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -24,6 +24,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity{
     private int currentposition;
     private int nextposition;
     private int backposition;
+    private boolean aBoolean=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,8 @@ public class MainActivity extends AppCompatActivity{
         final Button next=(Button)findViewById(R.id.next);
         final TextView tv1=(TextView)findViewById(R.id.tv1);
         final TextView tv2=(TextView)findViewById(R.id.tv2);
+        final Button moshi=(Button) findViewById(R.id.moshi);
+        moshi.setText("顺序播放");
         seekBar=(SeekBar)findViewById(R.id.seekbar);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -127,52 +131,22 @@ public class MainActivity extends AppCompatActivity{
         back.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Song song=list.get(backposition);
-                if(backposition==0){
-                    currentposition=backposition;
-                    nextposition=currentposition+1;
-                    backposition=list.size()-1;
-                }
-                else {
-                    currentposition = backposition;
-                    nextposition = currentposition + 1;
-                    backposition = currentposition - 1;
-                }
-                seekBar.setMax(song.duration);
-                tv1.setText(song.song);
-                tv2.setText(song.singer);
-                try {
-                    mediaPlayer.reset();
-                    mediaPlayer.setDataSource(song.path);
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        next.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Song song = list.get(nextposition);
-                if (nextposition==0) {
-                    currentposition=nextposition;
-                    backposition = list.size() - 1;
-                    nextposition+=1;
-                } else if(nextposition==list.size()-1){
-                    currentposition=nextposition;
-                    backposition=currentposition-1;
-                    nextposition=0;
-                }
-                else{
-                    currentposition = nextposition;
-                    backposition = currentposition - 1;
-                    nextposition = currentposition + 1;
-                }
+                if(aBoolean==false) {
+                    Song song = list.get(backposition);
+                    if (backposition == 0) {
+                        currentposition = backposition;
+                        nextposition = currentposition + 1;
+                        backposition = list.size() - 1;
+                    } else {
+                        currentposition = backposition;
+                        nextposition = currentposition + 1;
+                        backposition = currentposition - 1;
+                    }
                     seekBar.setMax(song.duration);
                     tv1.setText(song.song);
                     tv2.setText(song.singer);
                     try {
+                        mediaPlayer.stop();
                         mediaPlayer.reset();
                         mediaPlayer.setDataSource(song.path);
                         mediaPlayer.prepare();
@@ -181,12 +155,147 @@ public class MainActivity extends AppCompatActivity{
                         e.printStackTrace();
                     }
                 }
+                else{
+                    Random random=new Random();
+                    currentposition=random.nextInt(list.size());
+                    Song song=list.get(currentposition);
+                    seekBar.setMax(song.duration);
+                    tv1.setText(song.song);
+                    tv2.setText(song.singer);
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(song.path);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(aBoolean==false) {
+                    Song song = list.get(nextposition);
+                    if (nextposition == 0) {
+                        currentposition = nextposition;
+                        backposition = list.size() - 1;
+                        nextposition += 1;
+                    } else if (nextposition == list.size() - 1) {
+                        currentposition = nextposition;
+                        backposition = currentposition - 1;
+                        nextposition = 0;
+                    } else {
+                        currentposition = nextposition;
+                        backposition = currentposition - 1;
+                        nextposition = currentposition + 1;
+                    }
+                    seekBar.setMax(song.duration);
+                    tv1.setText(song.song);
+                    tv2.setText(song.singer);
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(song.path);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    Random random=new Random();
+                    currentposition=random.nextInt(list.size());
+                    Song song=list.get(currentposition);
+                    seekBar.setMax(song.duration);
+                    tv1.setText(song.song);
+                    tv2.setText(song.singer);
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(song.path);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
         });
+        moshi.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(aBoolean){
+                    aBoolean=false;
+                    Button button = findViewById(R.id.moshi);
+                    button.setText("顺序播放");
+                    return;
+                }
+                if(aBoolean==false){
+                    Button button = findViewById(R.id.moshi);
+                    button.setText("随机播放");
+                    aBoolean=true;
+                    return;
+                }
+            }
+        });
 
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                if(aBoolean==false){
+                    Song song=list.get(nextposition);
+                    currentposition=nextposition;
+                    if(currentposition==0){
+                        backposition=list.size()-1;
+                        nextposition=currentposition+1;
+                    }
+                    else if(currentposition==list.size()-1){
+                        backposition=currentposition-1;
+                        nextposition=0;
+                    }
+                    else{
+                        nextposition=currentposition+1;
+                        backposition=currentposition-1;
+                    }
+                    tv1.setText(song.song);
+                    tv2.setText(song.singer);
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(song.path);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    Random random=new Random();
+                    currentposition=random.nextInt(list.size());
+                    Song song=list.get(currentposition);
+                    tv1.setText(song.song);
+                    tv2.setText(song.singer);
+                    try {
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                        mediaPlayer.setDataSource(song.path);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
         seekProcess();
 
     }
+
 
     public void seekProcess(){
         Timer timer=new Timer();
